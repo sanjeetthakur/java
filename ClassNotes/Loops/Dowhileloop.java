2789b605e1af270ee9ac74893541086929db5434
Sanjeet Thakur <57172419+sanjeetthakur@users.noreply.github.com>
2025-02-18 23:28:23 +0530

    Add files via upload

---
+++ ClassNotes/Loops/Dowhileloop.java
@@ -0,0 +1,18 @@
+package ClassNotes.Loops;
+
+public class Dowhileloop{
+    public static void main(String[] args) {
+        int i = 12;
+        //1
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while (i<11);
+        //2
+        do{
+            System.out.println("High Command Ordered");
+        }
+        while (i<=11);
+    }
+}
---
+++ ClassNotes/Loops/Sunoffirstn.java
@@ -0,0 +1,14 @@
+package ClassNotes.Loops;
+import java.util.*;
+public class Sunoffirstn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum =0;
+        for (int i = 1; i <= n; i++){
+            sum=sum+i;
+            System.out.println(sum);   
+        }
+        sc.close();
+    }
+}
